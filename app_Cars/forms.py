d6a9b1e291151db90c1f42fd70f1bd625c0e8ef9
NatalyaKirsanova <mot_a@bk.ru>
2020-10-28 16:58:09 +0300

    change

--- app_Cars/forms.py
+++ app_Cars/forms.py
@@ -4,5 +4,5 @@ from django import forms
 class CarsFilterForms(forms.Form):
     search=forms.CharField(label='производитель/модель',required=False)
     search_year = forms.IntegerField(label='год выпуска больше', required=False)
-    search_transmission =forms.CharField(label='коробка передач', required=False)
+    # search_transmission =forms.CharField(label='коробка передач', required=False)
     # transmission
--- app_Cars/views.py
+++ app_Cars/views.py
@@ -16,23 +16,22 @@ def cars_view(request):
     cars=Car.objects.all()
     form = CarsFilterForms(request.GET)
     if form.is_valid():
-        # if form.cleaned_data["search"]:
-        #     cars= Car.objects.filter(Q(manufacturer__contains = form.cleaned_data["search"])|Q(modelCar__contains = form.cleaned_data["search"]))
-        # elif form.cleaned_data["search"] and  form.cleaned_data["search_year"] :
-        #     cars1 = Car.objects.filter(Q(manufacturer__contains=form.cleaned_data["search"]) | Q(modelCar__contains=form.cleaned_data["search"]))
-        #     cars=cars1.filter(Q(year_manufacture__gt=form.cleaned_data["search_year"]))
-        # # elif form.cleaned_data["search_transmission"]:
-        #     cars = Car.objects.filter(Q(transmission__contains=form.cleaned_data["search_transmission"]))
-        #
-        # else:
-        #     cars = Car.objects.all()
         if form.cleaned_data["search"]:
             cars= Car.objects.filter(Q(manufacturer__contains = form.cleaned_data["search"])|Q(modelCar__contains = form.cleaned_data["search"]))
-        if form.cleaned_data["search_year"]:
-            cars=cars.filter(year_manufacture__gt=form.cleaned_data["search_year"])
-        if form.cleaned_data["search_transmission"]:
-            cars = Car.objects.filter(Q(transmission__contains=form.cleaned_data["search_transmission"]))
 
+        if form.cleaned_data["search"] and form.cleaned_data["search_year"]:
+            cars = Car.objects.filter(Q(manufacturer__contains=form.cleaned_data["search"]) | Q(
+                modelCar__contains=form.cleaned_data["search"]))
+            cars =cars.filter(year_manufacture__gt=form.cleaned_data["search_year"])
+
+
+        if form.cleaned_data["search_year"]:
+            cars=Car.objects.filter(year_manufacture__gt=form.cleaned_data["search_year"])
+        # if form.cleaned_data["search_transmission"]:
+        #
+        #     cars = Car.objects.filter(transmission__contains=form.cleaned_data["search_transmission"])
+        # else:
+        #      cars = Car.objects.all()
 
 
 
